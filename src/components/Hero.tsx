b252f7aae62685a8dfba44674843d766927290c0
kethnal <kaseyvaldez@hotmail.com>
2023-09-18 23:45:10 -0700

    Set up font properties

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+
 function Hero() {
   return (
-    <div>
+    <div className="hero">
       <picture>
         <source media="(min-width: 1100px)" srcSet="homepage/hero-bg-desktop.jpg" />
         <source media="(min-width: 768px)" srcSet="homepage/hero-bg-tablet@2x.jpg" />
         <img
-          src="homepage/hero-bg-mobile.jpg"
+          src="homepage/hero-bg-mobile@2x.jpg"
           alt=""
         />
       </picture>
       <header>
+        <img src="logo.svg" alt="" />
         <h1>Exquisite dining since 1989</h1>
         <p>
           Experience our seasonal menu in beautiful country surroundings. Eat
