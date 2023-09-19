86280484f5ce7655086210a4582d41cd31c26af1
kethnal <kaseyvaldez@hotmail.com>
2023-09-19 01:37:13 -0700

    Style responsive banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Banner />
       <Footer />
     </main>
   )
---
+++ src/components/Banner.tsx
@@ -0,0 +1,24 @@
+function Banner() {
+  return (
+    <div className="banner">
+      <picture>
+        <source
+          media="(min-width: 1100px)"
+          srcSet="homepage/ready-bg-desktop@2x.jpg"
+        />
+        <source
+          media="(min-width: 768px)"
+          srcSet="homepage/ready-bg-tablet@2x.jpg"
+        />
+        <img src="homepage/ready-bg-mobile@2x.jpg" alt="" />
+      </picture>
+
+      <div className="banner_content | wrapper">
+        <span className="main-title">Ready to make a reservation?</span>
+        <button className="button">BOOK A TABLE</button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
