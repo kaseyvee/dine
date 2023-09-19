75fb9271f90167a8bea2bf54f0cbb1625e464f3b
kethnal <kaseyvaldez@hotmail.com>
2023-09-19 00:05:49 -0700

    Style hero component for mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,22 +4,28 @@ function Hero() {
   return (
     <div className="hero">
       <picture>
-        <source media="(min-width: 1100px)" srcSet="homepage/hero-bg-desktop.jpg" />
-        <source media="(min-width: 768px)" srcSet="homepage/hero-bg-tablet@2x.jpg" />
-        <img
-          src="homepage/hero-bg-mobile@2x.jpg"
-          alt=""
+        <source
+          media="(min-width: 1100px)"
+          srcSet="homepage/hero-bg-desktop.jpg"
         />
+        <source
+          media="(min-width: 768px)"
+          srcSet="homepage/hero-bg-tablet@2x.jpg"
+        />
+        <img src="homepage/hero-bg-mobile@2x.jpg" alt="" />
       </picture>
-      <header>
+
+      <div className="wrapper | hero_content">
         <img src="logo.svg" alt="" />
-        <h1>Exquisite dining since 1989</h1>
-        <p>
-          Experience our seasonal menu in beautiful country surroundings. Eat
-          the freshest produce from the comfort of our farmhouse.
-        </p>
-        <button>BOOK A TABLE</button>
-      </header>
+        <header>
+          <h1 className="hero-title">Exquisite dining <br/> since 1989</h1>
+          <p className="paragraph">
+            Experience our seasonal menu in beautiful country surroundings. Eat
+            the freshest produce from the comfort of our farmhouse.
+          </p>
+          <button className="button">BOOK A TABLE</button>
+        </header>
+      </div>
     </div>
   );
 }
